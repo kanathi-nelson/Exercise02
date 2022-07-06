f3d8aef695e4a0857bd47dfa2f0a1274afdc74f2
kanathi-nelson <nelsonkanathi@gmail.com>
2022-07-07 02:35:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long val;
+            Console.Write("Enter number: ");
+            string inputval = Console.ReadLine();
+            try
+            {
+                val = Convert.ToInt64(inputval);
+                string mynumber = Exercise01.Class1.Towards(val);
+                Console.WriteLine(mynumber);
+
+                Console.WriteLine("Exiting...");
+            }
+            catch
+            {
+                Console.WriteLine("Enter a valid string!");
+                throw;
+            }
+            
+        }
+    }
+}
